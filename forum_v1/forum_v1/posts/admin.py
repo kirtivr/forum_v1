81a2997da2559ec63f87783e45824e18ef1f1441
kirtivr <kirtivr.bit@gmail.com>
2023-05-18 16:05:28 +0530

    Create simple models for Post and Author. Allow admins to add posts and authors for testing

--- forum_v1/forum_v1/posts/admin.py
+++ forum_v1/forum_v1/posts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Post
+
+class AuthorInstanceInline(admin.TabularInline):
+    model = Author
+    extra = 0
+
+class PostsInline(admin.TabularInline):
+    model = Post
+    extra = 0
+
+# Define the admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_name', 'email', 'designation', 'commends', 'replies')
+
+    # will display horizontally if you further group them in a tuple (as shown in the "date" fields below)
+    fields = ['last_name', 'first_name', 'display_name', 'email', 'designation', ('commends', 'replies')]
+
+    inlines = [PostsInline]
+
+class PostsInline(admin.TabularInline):
+    model = Post
+    extra = 0
+
+# Register the Admin classes for Post using the decorator
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'topic', 'contents', 'commends', 'replies')
--- forum_v1/forum_v1/posts/models.py
+++ forum_v1/forum_v1/posts/models.py
@@ -40,16 +40,15 @@ class Post(models.Model):
 
     TOPICS = (
         ('m', 'Maintenance'),
-        ('o', 'Tankers'),
-        ('a', 'Suez Max'),
-        ('r', 'Engine Room'),
+        ('t', 'Tankers'),
+        ('s', 'Suez Max'),
+        ('e', 'Engine Room'),
     )
 
     topic = ArrayField(models.CharField(max_length=200, choices=TOPICS), blank=True)
 
-    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Foreign Key used because post can only have one author, but authors can have multiple posts.
     # Otherwise this would have been a ManyToMany field.
-    # Author is a string rather than an object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     date_posted = models.DateTimeField(null=True, blank=True, auto_now=True)
     contents = models.CharField(null=True, blank=True)
@@ -60,18 +59,12 @@ class Post(models.Model):
         """Returns the URL to access a particular post."""
         return reverse('post-detail', args=[str(self.id)])    
 
-
     class Meta:
         ordering = ['date_posted']
 
-    @property
-    def is_overdue(self):
-        """Determines if the book is overdue based on due date and current date."""
-        return bool(self.due_back and date.today() > self.due_back)
-
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.id} ({self.book.title})'
+        return f'{self.id} ({self.title} by {self.author})'
 
 def ready():
     from django.contrib.auth.models import User
